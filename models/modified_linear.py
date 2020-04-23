0c950fac95fa2336bd2e1a056318e3b3eeca1dc8
Yaoyao Liu <yaoyao.liu@mpi-inf.mpg.de>
2020-04-23 22:32:07 +0200

    Update modified_linear.py

--- models/modified_linear.py
+++ models/modified_linear.py
@@ -8,7 +8,7 @@
 ## This source code is licensed under the MIT-style license found in the
 ## LICENSE file in the root directory of this source tree
 ##+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-""" Modified linear layer. """
+"""Modified linear layer."""
 import math
 import torch
 from torch.nn import Module
