d271ed9299801c786e948c60d2da22705e07daac
Yaoyao Liu <yaoyao.liu@mpi-inf.mpg.de>
2020-04-23 22:32:23 +0200

    Update modified_resnet_cifar.py

--- models/modified_resnet_cifar.py
+++ models/modified_resnet_cifar.py
@@ -8,7 +8,7 @@
 ## This source code is licensed under the MIT-style license found in the
 ## LICENSE file in the root directory of this source tree
 ##+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-""" Modified ResNet. """
+"""Modified ResNet."""
 import math
 import torch.nn as nn
 import torch.utils.model_zoo as model_zoo
